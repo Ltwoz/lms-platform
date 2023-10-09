2e09d3c8b3d6733039ba719e477588dc013cb46d
Ltwoz <warrapat.102@gmail.com>
2023-10-09 23:46:35 +0700

    feat: daily update

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -0,0 +1,7 @@
+import { db } from "@/lib/db";
+
+const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
+  return <div>Course Id: {params.courseId}</div>;
+};
+
+export default CourseIdPage;
--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,15 +1,14 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 import { Button } from "./ui/button";
 import { LogOut } from "lucide-react";
-import Link from "next/link";
 
 const NavbarRoutes = () => {
   const pathname = usePathname();
-  const router = useRouter();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
   const isPlayerPage = pathname?.includes("/chapter");
@@ -17,10 +16,12 @@ const NavbarRoutes = () => {
   return (
     <div className="flex gap-x-2 ml-auto">
       {isTeacherPage || isPlayerPage ? (
-        <Button>
-          <LogOut className="h-4 w-4 mr-2" />
-          Exit
-        </Button>
+        <Link href="/">
+          <Button size="sm" variant="ghost">
+            <LogOut className="h-4 w-4 mr-2" />
+            Exit
+          </Button>
+        </Link>
       ) : (
         <Link href="/teacher/courses">
           <Button size="sm" variant="ghost">
@@ -28,7 +29,7 @@ const NavbarRoutes = () => {
           </Button>
         </Link>
       )}
-      <UserButton />
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
